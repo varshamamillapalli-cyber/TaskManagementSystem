8f27dfcd2b510d46974b267d78f9fc9fc3941a66
varshamamillapalli-cyber <varshamamillapalli@gmail.com>
2026-06-15 16:02:24 +0530

    Create HomeController.java

---
+++ springboot-backend/taskmanager/taskmanager/src/main/java/com/example/taskmanager/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.taskmanager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Task Manager Spring Boot Backend Running";
+    }
+}
